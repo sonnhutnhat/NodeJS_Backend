922b8e10e116037b72ed0b20296bf274b0ca9f72
sonnhutnhat <nguyenkienson3010@gmail.com>
2024-09-18 14:14:04 +0700

    cors

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -15,7 +15,7 @@ let handleLogin = async (req, res) => {
 
     return res.status(200).json({
         errCode: userData.errCode,
-        message: userData.errMesage,
+        message: userData.errMessage,
         user: userData.user ? userData.user : {}
     });
 }
--- src/server.js
+++ src/server.js
@@ -3,14 +3,15 @@ import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initWebRoutes from "./route/web";
 import connectBD from "./config/connectDB";
+import cors from 'cors';
 
 require('dotenv').config();
 
 let app = express();
-
+app.use(cors({ credentials: true, origin: true }));
 //config app
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 viewEngine(app);
 initWebRoutes(app);
@@ -22,5 +23,5 @@ let port = process.env.PORT || 6969;
 
 app.listen(port, () => {
     //call back 
-    console.log("Backend Nodejs is running on the port:  "+port);
+    console.log("Backend Nodejs is running on the port:  " + port);
 });
